e2ee0c43ee395239089595734569d5db7a80e509
Atharvahambarde <80406458+Atharvahambarde@users.noreply.github.com>
2025-01-06 15:50:59 +0530

    Initail Commit

---
+++ MyProject/src/library/MyLibrary.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "MyLibrary.h"
+
+void printMessage() {
+    std::cout << "Hello from MyLibrary!" << std::endl;
+}
---
+++ MyProject/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "MyLibrary.h"
+
+int main() {
+    std::cout << "Hello from main!" << std::endl;
+    printMessage();
+    return 0;
+}
